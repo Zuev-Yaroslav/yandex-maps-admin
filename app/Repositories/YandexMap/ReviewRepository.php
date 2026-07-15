27c9edb5fd7b6abbc45b37661e3c13ced74dab3b
Zuev-Yaroslav <zuevyaroslav937@gmail.com>
2026-07-15 20:28:04 +0300

    fix ReviewRepository.php

--- app/Repositories/YandexMap/ReviewRepository.php
+++ app/Repositories/YandexMap/ReviewRepository.php
@@ -10,23 +10,23 @@ use App\Services\YandexMapReviewService;
 
 class ReviewRepository
 {
-    public static function updateOrCreate(array $data): Review
+    public static function updateOrCreate(array $data)
     {
         return Review::updateOrCreate(
             ['reviewId' => $data['reviewId']],
             [
-                'author' => $data['author'],
-                'businessComment' => $data['businessComment'],
-                'businessId' => $data['businessId'],
-                'photos' => $data['photos'],
-                'rating' => $data['rating'],
-                'reactions' => $data['reactions'],
-                'reviewId' => $data['reviewId'],
-                'text' => $data['text'],
-                'textLanguage' => $data['textLanguage'],
-                'textTranslations' => $data['textTranslations'],
-                'updatedTime' => $data['updatedTime'],
-                'videos' => $data['videos'],
+                'author' => $data['author'] ?? '',
+                'businessComment' => $data['businessComment'] ?? '',
+                'businessId' => $data['businessId'] ?? '',
+                'photos' => $data['photos'] ?? '',
+                'rating' => $data['rating'] ?? '',
+                'reactions' => $data['reactions'] ?? '',
+                'reviewId' => $data['reviewId'] ?? '',
+                'text' => $data['text'] ?? '',
+                'textLanguage' => $data['textLanguage'] ?? '',
+                'textTranslations' => $data['textTranslations'] ?? '',
+                'updatedTime' => $data['updatedTime'] ?? '',
+                'videos' => $data['videos'] ?? '',
             ],
         );
     }
